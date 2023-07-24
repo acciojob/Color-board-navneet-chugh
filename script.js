6bcc1c24a2a3b9ecba88d2f04d3f85d976eb8437
navneet-chugh <navneetchugh15@gmail.com>
2023-07-24 20:06:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const board = document.querySelector('.board');
+  const container = document.querySelector('.container');
   const numBoxes = 800;
   const colors = ['#ffcc00', '#00aaff', '#ff6666', '#66cc66', '#cc66cc']; // Add more colors as needed
 
   for (let i = 0; i < numBoxes; i++) {
-    const box = document.createElement('div');
-    box.classList.add('box');
-    board.appendChild(box);
+    const square = document.createElement('div');
+    square.classList.add('square');
+    container.appendChild(square);
 
     // Add event listener for hover effect
-    box.addEventListener('mouseover', function() {
+    square.addEventListener('mouseover', function() {
       const randomColor = colors[Math.floor(Math.random() * colors.length)];
-      box.style.backgroundColor = randomColor;
+      square.style.backgroundColor = randomColor;
     });
 
-    // Reset the box color when mouse leaves the box
-    box.addEventListener('mouseout', function() {
-      box.style.backgroundColor = '#ffffff';
+    // Reset the square color when mouse leaves the square
+    square.addEventListener('mouseout', function() {
+      square.style.backgroundColor = '#ffffff';
     });
   }
 });
