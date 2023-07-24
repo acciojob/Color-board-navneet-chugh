d6972fccaf9b2ea34306fc1d4fdf8741b780e3d6
navneet-chugh <navneetchugh15@gmail.com>
2023-07-24 20:14:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const container = document.querySelector('.container');
   const numBoxes = 800;
   const colors = ['#ffcc00', '#00aaff', '#ff6666', '#66cc66', '#cc66cc']; // Add more colors as needed
+	cy.get('.square').trigger('mouseover', { force: true });
 
   for (let i = 0; i < numBoxes; i++) {
     const square = document.createElement('div');
